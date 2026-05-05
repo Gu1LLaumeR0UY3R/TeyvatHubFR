fdf388625fd3bc98508fc1dc944c109f5b577309
Guillaume <gui.rouyer@gmail.com>
2026-05-05 11:39:36 +0200

    chore: suppression fichiers debug temporaires

--- debug_table_names.php
+++
@@ -1,6 +0,0 @@
-<?php
-$pdo = new PDO('mysql:host=127.0.0.1;dbname=genshin_db', 'root', '');
-$stmt = $pdo->query("DESCRIBE rareté");
-foreach ($stmt->fetchAll(PDO::FETCH_ASSOC) as $row) {
-    echo $row['Field'] . " - " . $row['Type'] . "\n";
-}
